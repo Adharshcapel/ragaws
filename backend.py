407bf6a30df211e1f4ce61b441b9779925c4362c
AdharshCapel <adharshsobhanan@gmail.com>
2026-05-01 19:29:33 +0530

    apicommits

---
+++ backend.py
@@ -0,0 +1,38 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KNOWLEDGE_BASE_ID = os.getenv("KNOWLEDGE_BASE_ID")
+MODEL_ARN = os.getenv("MODEL_ARN")
+REGION = os.getenv("AWS_DEFAULT_REGION")
+
+client = boto3.client(
+    "bedrock-agent-runtime",
+    region_name=REGION
+)
+
+def query_kb(question):
+    response = client.retrieve_and_generate(
+        input={"text": question},
+        retrieveAndGenerateConfiguration={
+            "type": "KNOWLEDGE_BASE",
+            "knowledgeBaseConfiguration": {
+                "knowledgeBaseId": KNOWLEDGE_BASE_ID,
+                "modelArn": MODEL_ARN
+            }
+        }
+    )
+
+    answer = response["output"]["text"]
+
+    sources = []
+    try:
+        for item in response["citations"]:
+            for ref in item["retrievedReferences"]:
+                sources.append(ref["content"]["text"])
+    except:
+        pass
+
+    return answer, sources
---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from backend import query_kb
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "RAG API Running"}
+
+@app.post("/query")
+def query(question: str):
+    answer, sources = query_kb(question)
+    return {"answer": answer, "sources": sources}
